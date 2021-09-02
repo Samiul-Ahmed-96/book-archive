f143d345f2d88951e2bf149d1bf80fc23278a085
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-09-02 16:37:46 +0600

    project done

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 
 const error = document.getElementById('error-show');
+const totalItem = document.getElementById('total-item-show');
 //get the books from api
 const loadBooks = () =>{
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
+    totalItem.innerText = '';
     //clear search field value
     searchField.value = '';
     const url = `http://openlibrary.org/search.json?q=${searchText}`;
@@ -24,6 +26,7 @@ const loadBooks = () =>{
         error.innerText = '';
     }
     getBooks(data.docs);  
+   
     })
     
 }
@@ -33,12 +36,14 @@ const getBooks = (books) =>{
     
     const booksArray = books;
     const bookLength = books.length;
+    console.log(bookLength)
+
+    totalItem.innerText ='Total Item Found : '+bookLength;
     const booksContainer = document.getElementById('books-container');
     //Clear the container
     booksContainer.textContent = ' ';
     //get the book using for each
     booksArray.forEach(book => {
-        console.log(book)
         error.innerText = '';
         //create a new div
         const bookDiv = document.createElement('div');
@@ -46,11 +51,10 @@ const getBooks = (books) =>{
         //insert the value in the new div
         bookDiv.innerHTML = `
         <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" alt="">
-        <h2>Name:${book.title}</h2>
-        <h3>Author:${book.author_name[0]}</h3>
-        <h5>Publisher:${book.publisher[0]}</h5>
+        <h2>Name: ${book.title}</h2>
+        <h3>Author: ${book.author_name[0]}</h3>
+        <h5>Publisher: ${book.publisher[0]}</h5>
         <h4>First Publish : ${book.first_publish_year}</h4>
-        
         `
         //Insert the div into books 
         booksContainer.appendChild(bookDiv);
